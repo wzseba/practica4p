7eb5681429a49bd80f6b01acb32e21742168f805
wzseba <wzseba@gmail.com>
2024-11-21 19:42:05 -0300

    delete: metodos innecesarios

--- src/television/Programa.java
+++ src/television/Programa.java
@@ -2,37 +2,18 @@ package television;
 
 public class Programa {
 
+	private int canal;
 	private String programa;
-	private int numCanal;
 	private double rating;
 
-	public Programa(int numCanal, String programa, double rating) {
-		this.numCanal = numCanal;
+	public Programa(int canal, String programa, double rating) {
+		this.canal = canal;
 		this.programa = programa;
 		this.rating = rating;
-	}
-
-	public void setCanal(int canal) {
-		this.numCanal = canal;
-	}
-
-	public int getCanal() {
-		return numCanal;
-	}
-
-	public void setPrograma(String programa) {
-		this.programa = programa;
-	}
-
-	public String getPrograma() {
-		return programa;
 	}
 
 	public double getRating() {
 		return rating;
 	}
 
-	public void setRating(double rating) {
-		this.rating = rating;
-	}
 }
